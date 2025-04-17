6410160b778d57d216a3462df1faa7331ee121c7
Aparna Paul <aparnapaul55@gmail.com>
2025-04-17 19:52:25 +0930

    --

--- controllers/userController.js
+++ controllers/userController.js
@@ -95,7 +95,7 @@ export const loginUser = async (req, res, next) => {
     res.cookie("token", token, {
       sameSite: NODE_ENV === "production" ? "None" : "Lax",
       secure: NODE_ENV === "production",
-      httpOnly: NODE_ENV === "production",
+      httpOnly: true
     });
 
     delete userExist._doc.password;
